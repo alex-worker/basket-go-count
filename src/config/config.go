b6a8f414f1873e069f9df0df3286c7eb05e6119f
Alexey Polyanin <pa@green-g.ru>
2022-04-28 18:19:10 +0300

    some config.go

--- src/config/config.go
+++ src/config/config.go
@@ -1,32 +1,20 @@
 package config
 
 import (
-	"flag"
-	"fmt"
 	"github.com/spf13/viper"
-	"log"
-	"os"
 )
 
-const deafaultConnectString = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+const connectionStringName = "DATABASE_CONNECTION"
+const defaultConnectString = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
 
 func Read() {
-	var name string
-	flag.StringVar(&name, "uri", deafaultConnectString, "Specify name. Default is admin.")
-
-	flag.Usage = func() {
-		fmt.Printf("Usage of basket-go-count: \n")
-		fmt.Printf("./basket-go-count -uri <db connect uri>\n")
-		fmt.Printf("<db connect uri> = <postgres>://<username>:<password>@<host>:<port>/<database>[?sslmode=<sslmode>]\n")
+	viper.SetDefault("db.uri", defaultConnectString)
+	err := viper.BindEnv(connectionStringName)
+	if err != nil {
+		panic(error.Error)
 	}
-	flag.Parse()
-
-	log.Print(name)
 }
 
 func ReadConnectionString() string {
-	log.Print(os.Getenv("uri"))
-	return viper.GetString("db.Uri")
-	//connStr := "host=balarama.db.elephantsql.com port=5432 user=mmxxboti password=18iR36iF2GJi1kY4ki7HfHw4Idamq1_M dbname=mmxxboti"
-	//const connStr = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	return viper.GetString("db.uri")
 }
