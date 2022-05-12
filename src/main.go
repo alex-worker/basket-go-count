8ae9533d1d886fd8ceef6eb3cff2eb5f23613b11
sir Random <RandomAmbersky@gmail.com>
2022-05-12 20:24:29 +0300

    update

--- src/main.go
+++ src/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	_ "github.com/lib/pq" // add this
+	"log"
+)
 
 func main() {
 	log.Println("Hello world")
+
+	connStr := "postgresql://postgres:postgres@localhost:5432/todos?sslmode=disable"
+	// Connect to database
+	_, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Ok")
 }
